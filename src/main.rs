ca364be0b733c9c5157e6e1d1fd96cae2aece4e9
Daniel Penedo <pen22217@uvg.edu.gt>
2024-07-24 19:15:40 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,52 +35,8 @@ fn main() {
         (193, 383)
     ];
 
-    let poly2: Vec<(usize, usize)> = vec![
-        (321, 335),
-        (288, 286),
-        (339, 251),
-        (374, 302)
-    ];
-
-    let poly3: Vec<(usize, usize)> = vec![
-        (377, 249),
-        (411, 197),
-        (436, 249)
-    ];
-
-    let poly4: Vec<(usize, usize)> = vec![
-        (413, 177),
-        (448, 159),
-        (502, 88),
-        (553, 53),
-        (535, 36),
-        (676, 37),
-        (660, 52),
-        (750, 145),
-        (761, 179),
-        (672, 192),
-        (659, 214),
-        (615, 214),
-        (632, 230),
-        (580, 230),
-        (597, 215),
-        (552, 214),
-        (517, 144),
-        (466, 180)
-    ];
-
-    let poly5: Vec<(usize, usize)> = vec![
-        (682, 175),
-        (708, 120),
-        (735, 148),
-        (739, 170)
-    ];
-
     framebuffer.polygon(&poly1, color1, border);
-    framebuffer.polygon(&poly2, color2, border);
-    framebuffer.polygon(&poly3, color3, border);
-    framebuffer.polygon(&poly4, color4, border);
-    framebuffer.polygon(&poly5, color5, border);
+
 
     let _ = framebuffer.render_buffer("output.bmp");
 
